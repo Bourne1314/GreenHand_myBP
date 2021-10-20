063d37d8ff56b4821d23eea812b545bc99a6570b
JR-zhangzhaojun <78338513@qq.com>
2021-10-20 18:12:28 +0800

    【TEST】增加其他测试内容

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,14 +3,17 @@ package com.example.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @MapperScan("com.example.demo.mapper")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println("这是我的demo初始化项目！");
+        //test
     }
-
 }
