afde1960e456fbdce1a8ea467aad18c55118bf8a
78338513@qq.com <78338513@qq.com>
2021-12-02 18:40:49 +0800

    removenacos

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,9 +1,8 @@
 package com.example.demo;
 
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.demo.DAO.GreenHand;
 import com.example.demo.Mapper.GreenHandMapper;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
